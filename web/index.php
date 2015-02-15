1f8b23f14ff213a1b577b8ccd8bc97d2364a6389
Teddy Wong <teddy.wong@ssense.com>
2015-02-14 19:02:03 -0500

    Added validation when JSON file is missing id

--- web/index.php
+++ web/index.php
@@ -10,49 +10,112 @@ require_once PATH_CONFIGS;
 $app = new Phalcon\Mvc\Micro();
 
 $app->post('/data_bag/decrypt', function () use ($app) {
-  $fp = fopen(TMPFILE, "w");
- 
-  $l = json_encode($app->request->getJsonRawBody());
-  $jdec = json_decode($l,true);
- 
-  fputs($fp, $l);
-  fclose($fp);
 
+  $enc_databag = "";
+  $status_code='200';
+  $status_message='OK';
+
+  $databag = json_encode($app->request->getJsonRawBody());
+
+  try
+  {
+    $databag_id = json_decode($databag,true)['id'];
+  } catch (Exception $e)
+  {
+      $enc_databag = "[ERROR] Data bag is missing an id entry.";
+  }
+
+  if ( is_null($databag_id) && empty($databag_id) )
+  {
+    $status_code='409';
+    $status_message='MISSING ID';
+    $enc_databag = "[ERROR] Data bag is missing an id entry.\n";
+  }
+  else
+  {  
+    $fp = fopen(TMPFILE, "w");
+    fputs($fp, $databag);
+    fclose($fp);
+
+    // Create Temporary Data Bag on Chef Server
+    exec("knife data bag create " . PID . " " . KNIFE_OPT , $empty , $retval);
+
+    // Import Data Bag 
+    exec("knife data bag from file ". PID . " " . TMPFILE . " " . KNIFE_OPT, $empty, $retval);
+
+    // List Data Bag in decrypted format
+    $dec_databag = shell_exec("knife data bag show " . PID . " " . $databag_id . " -Fj --secret-file=" . PATH_CHEF . "encrypted_data_bag_secret ". KNIFE_OPT );
+
+    // Delete the Data Bag from Chef Server
+    exec("echo y | knife data bag delete " . PID . " " . KNIFE_OPT, $empty, $retval);
   
-  exec("knife data bag create " . PID , $empty , $retval);
-  exec("knife data bag from file ". PID . " " . TMPFILE, $empty, $retval);
-  $dec_databag = shell_exec("knife data bag show " . PID . " " . $jdec['id'] . " -Fj --secret-file=" . PATH_CHEF . "encrypted_data_bag_secret");
-  exec("echo y | knife data bag delete " . PID, $empty, $retval);
+    // Delete temporary file
+    unlink(TMPFILE);
+  }
 
-  $response = new Phalcon\Http\Response();
+  // Prepare Response
   $response->setHeader("Content-Type", "application/json");
-  $response->setStatusCode(200, "OK");
+  $response->setStatusCode($status_code, $status_message);
   $response->setContent("$dec_databag");
-  $response->send();
+  $response->send(); 
 
 });
 
 $app->post('/data_bag/encrypt', function () use ($app) {
-
-  $fp = fopen(TMPFILE, "w");
- 
-  $l = json_encode($app->request->getJsonRawBody());
-  $jdec = json_decode($l,true);
-  fputs($fp, $l);
-  fclose($fp);
-
   
-  exec("knife data bag create " . PID , $empty , $retval);
-  exec("knife data bag from file ". PID . " " . TMPFILE . " --secret-file=" . PATH_CHEF . "encrypted_data_bag_secret", $empty, $retval);
-  $enc_databag = shell_exec("knife data bag show " . PID . " " . $jdec['id'] . " -Fj");
-  exec("echo y | knife data bag delete " . PID, $empty, $retval);
+  $enc_databag = ""; 
+  $status_code='200';
+  $status_message='OK';
 
   $response = new Phalcon\Http\Response();
+
+  $databag = json_encode($app->request->getJsonRawBody());
+
+  try 
+  {
+    $databag_id = json_decode($databag,true)['id'];
+  } catch (Exception $e)
+  {
+      $enc_databag = "[ERROR] Data bag is missing an id entry.";
+  }
+
+
+  if ( is_null($databag_id) && empty($databag_id) )
+  {
+    $status_code='409';
+    $status_message='MISSING ID';      
+    $enc_databag = "[ERROR] Data bag is missing an id entry.\n"; 
+  }
+  else
+  {
+    // Write data bag on disk
+    $fp = fopen(TMPFILE, "w");
+    fputs($fp, $databag);
+    fclose($fp);
+
+    // Create Temporary Data Bag on Chef Server
+    exec("knife data bag create " . PID . " " . KNIFE_OPT, $empty , $retval);
+
+    // Import and Encrypt data bag into temporary databag location 
+    exec("knife data bag from file ". PID . " " . TMPFILE . " --secret-file=" . PATH_CHEF . "encrypted_data_bag_secret " . KNIFE_OPT, $empty, $retval);
+
+    // Extract Encrypted databag in JSON format
+    $enc_databag = shell_exec("knife data bag show " . PID . " " . $databag_id . " -Fj " . KNIFE_OPT);
+
+    // Delete the Data Bag from Chef Server
+    exec("echo y | knife data bag delete " . PID . " " . KNIFE_OPT, $empty, $retval);
+
+    // Delete temporary file
+    unlink(TMPFILE);
+  }
+
+  // Prepare Response
   $response->setHeader("Content-Type", "application/json");
-  $response->setStatusCode(200, "OK");
+  $response->setStatusCode($status_code, $status_message);
   $response->setContent("$enc_databag");
   $response->send();
 
 });
 
 $app->handle();
+
